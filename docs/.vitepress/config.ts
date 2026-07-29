29e42466f28a607adaccc2c5756ff4ed38891890
EthanKuai <ethankuaiek@gmail.com>
2026-07-29 14:51:21 +0800

    feat(home): Add email icon
    Signed-off-by: EthanKuai <ethankuaiek@gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -77,6 +77,13 @@ export default withMermaid(
 
       socialLinks: [
         {
+          icon: {
+            svg: '<svg width="1em" height="1em" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" ><path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" /><path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" /></svg>',
+          },
+          link: 'mailto:ros-i_asia@a-star.edu.sg',
+          ariaLabel: 'email',
+        },
+        {
           icon: 'github',
           link: 'https://github.com/ros-industrial/rmf_industrial',
         },
